00a42bc85821eeeb1bfc656cd4760ae118058581
xuehuihe.java <xuehuihe.java@gmail.com>
2015-11-22 12:00:00 +0800

    modify find word functional

--- source/vendors/english.js
+++ source/vendors/english.js
@@ -5,12 +5,19 @@ $(function(){
       myVoice = document.createElement("audio");
     }
     var p = $(event.target).parent();
-    var text = p.text().trim();
-    var index = text.search(' ');
-    text = text.substr(0,index).trim();
+    var text = findWords(p.text());
     myVoice.src = "http://dict.youdao.com/dictvoice?audio=" + text +"&type=2";
     myVoice.play();
     return false;
   });
 }
 );
+function findWords(text){
+  text = text.trim();
+  var index = text.search('\\[');
+  if(index != -1){
+    text = text.substr(0,index).trim();
+  }
+  text = text.replace(/\s+/,'%2B');
+  return text;
+}
