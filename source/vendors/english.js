4b05087f43887f8e528c49b07dd55475e645d10c
xuehuihe.java <xuehuihe.java@gmail.com>
2016-01-17 22:01:10 +0800

    modify english.js

--- source/vendors/english.js
+++ source/vendors/english.js
@@ -1,15 +1,59 @@
 $(function(){
   var myVoice = null;
+  var cursor = null;
+
   $('a[href="#v"]').click(function(event){
     if(myVoice == null){
       myVoice = document.createElement("audio");
+      addWordCursor();
     }
-    var p = $(event.target).parent();
-    var text = findWords(p.text());
-    myVoice.src = "http://dict.youdao.com/dictvoice?audio=" + text +"&type=2";
-    myVoice.play();
+    cursor = $(event.target).parent();
+    readCursor();
     return false;
   });
+  
+  function readCursor(){
+    var text = findWords(cursor.text());
+    myVoice.src = "http://dict.youdao.com/dictvoice?audio=" + text +"&type=2";
+    myVoice.play();
+  }
+  
+  function readNext(){
+    cursor = cursor.next();
+    readCursor();
+  }
+  
+  function readPrev(){
+    cursor = cursor.prev();
+    readCursor();
+  }
+  
+  function readRepeat(){
+    readCursor();
+  }
+  
+  function addWordCursor(){
+    $(document).keydown(function(event) {
+      switch(event.which) {
+          case 37: // left
+            readPrev();
+          break;
+
+          case 38: // up
+            readRepeat();
+          break;
+
+          case 39: // right
+            readNext();
+          break;
+
+          case 40: // down
+            readRepeat();
+          break;
+      }
+      return false;
+    });
+  }
 }
 );
 function findWords(text){
